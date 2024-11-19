a8c43ba5fac7e66c096a09c0e8c5375173a70f8d
RRB-Rafael <Rafael.bastos9928@gmail.com>
2024-11-19 12:24:06 -0300

    v1: Operadores

---
+++ Operadores/oper_02.py
@@ -0,0 +1,6 @@
+#Crie um programa que peça ao usuário um número e o eleve ao quadrado usando o operador de exponenciação(**).
+x = int(input("Insira um número: "))
+exp = x**2
+print(f"{x}² = {exp}")
+
+print("FIM-------------------------------------")
---
+++ Operadores/oper_3.py
@@ -0,0 +1,6 @@
+#Solicite ao usuário o lado de um quadrado e calcule sua área usando a fórmula: Área = lado².
+lado = int(input("Informe o lado de um quadrado: "))
+area= lado **2
+print(f"A área do seu quadrado é: {area}")
+
+print("FIM-------------------------------------")
