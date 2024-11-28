18c92554d1a0fa28b90c68aeeb6d16e78e8605f4
RRB-Rafael <Rafael.bastos9928@gmail.com>
2024-11-28 10:39:44 -0300

    V1

--- Operadores/ope_01.py
+++ Operadores/ope_01.py
@@ -1,20 +1,13 @@
-# Solicita dois números ao usuário
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
-
-# Calcula as operações
+#Escreva um programa que solicite dois números ao usuário e exiba: a soma dos dois números, a subtração do primeiro número pelo segundo, a multiplicação dos dois números e a divisão do primeiro número pelo segundo.
+num1 = int(input("Insira o primeiro num: "))
+num2 = int(input("Insira o segundo num: "))
 soma = num1 + num2
-subtracao = num1 - num2
-multiplicacao = num1 * num2
+sub = num1-num2
+mult = num1*num2
+div= num1 / num2
+print(f"O resultado da soma destes números é: {soma}")
+print(f"O resultado da subtração destes números é: {sub}")
+print(f"O resultado da multiplicação destes números é: {mult}")
+print(f"O resultado da divisão destes números é: {div}")
 
-# Verifica se o segundo número é diferente de zero antes de dividir
-if num2 != 0:
-    divisao = num1 / num2
-else:
-    divisao = "Indefinida (não é possível dividir por zero)"
-
-# Exibe os resultados
-print(f"Soma: {soma}")
-print(f"Subtração: {subtracao}")
-print(f"Multiplicação: {multiplicacao}")
-print(f"Divisão: {divisao}")
+print("FIM-------------------------------------")
